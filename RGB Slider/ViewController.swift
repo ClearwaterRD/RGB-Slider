88a2aee3670e596a6ea1550946bb5ceb64dbaf27
RD <dybedy10@gmail.com>
2024-07-09 10:33:29 -0500

    The logic of the sliders has been configured

--- RGB Slider/ViewController.swift
+++ RGB Slider/ViewController.swift
@@ -17,31 +17,62 @@ final class ViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
    
-    @IBOutlet weak var showRgbColorView: UIView!
+    @IBOutlet weak var rgbColorView: UIView!
     
-    private var redColorValue: CGFloat = 0.0
-    private var greenColorValue: CGFloat = 0.0
-    private var blueColorValue: CGFloat = 0.0
+    private var redColorValue: CGFloat {
+        CGFloat(redSlider.value)
+    }
     
+    private var greenColorValue: CGFloat {
+        CGFloat(greenSlider.value)
+    }
+    
+    private var blueColorValue: CGFloat {
+        CGFloat(blueSlider.value)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        showRgbColorView.layer.cornerRadius = 10
-        
-        
+        rgbColorView.layer.borderWidth = 2.0
+        rgbColorView.layer.borderColor = UIColor.white.cgColor
+        rgbColorView.layer.cornerRadius = 10
     }
 
     @IBAction func redSliderAction() {
         redColorLabel.text = String(format: "%.2f", redSlider.value)
+        getRgbColor(
+            with: redColorValue,
+            green: greenColorValue,
+            blue: blueColorValue
+        )
     }
     
     @IBAction func greenSliderAction() {
         greenColorLabel.text = String(format: "%.2f", greenSlider.value)
+        getRgbColor(
+            with: redColorValue,
+            green: greenColorValue,
+            blue: blueColorValue
+        )
     }
     
     @IBAction func blueSliderAction() {
         blueColorLabel.text = String(format: "%.2f", blueSlider.value)
+        getRgbColor(
+            with: redColorValue,
+            green: greenColorValue,
+            blue: blueColorValue
+        )
+    }
+    
+    
+    private func getRgbColor(with red: CGFloat, green: CGFloat, blue: CGFloat) {
+        rgbColorView.backgroundColor = UIColor(
+            red: red,
+            green: green,
+            blue: blue,
+            alpha: 1.0
+        )
     }
 }
 
